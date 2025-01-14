d0fdc4889bee17db17627fd53c2b65566c3cee58
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2025-01-15 02:49:09 +0500

    STATICPATH

--- marketplace_server/settings.py
+++ marketplace_server/settings.py
@@ -156,10 +156,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
-STATIC_ROOT = '/marketplace/static/'
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/marketplace/media/'
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
