a1c84491ffc80f61bbad0effda5b4563abcddc81
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2025-02-24 12:12:45 +0500

    FRONTEND UPDATE

--- src/apps/product/models.py
+++ src/apps/product/models.py
@@ -8,6 +8,8 @@ from io import BytesIO
 from django.core.files import File
 from ckeditor.fields import RichTextField
 
+from src.apps.wishlist.models import WishListProduct
+
 
 # PRODUCT PARENT CATEGORY
 
@@ -46,24 +48,37 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
-    brand = models.CharField(max_length=255,null=True, blank=True)
+    brand = models.CharField(max_length=255, null=True, blank=True)
     sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, related_name='products')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.PositiveIntegerField(default=0)
     sku = models.CharField(max_length=100, unique=True)
-    barcode = models.CharField(max_length=100, unique=True,blank=True,null=True)
+    barcode = models.CharField(max_length=100, unique=True, blank=True, null=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='review')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_featured = models.BooleanField(default=False)
     sales_count = models.PositiveIntegerField(default=0)
-    content = RichTextField(blank=True,null=True)
+    content = RichTextField(blank=True, null=True)
     added_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+    def is_in_wishlist(self, user):
+        """
+        Check if the product is in the user's wishlist.
+
+        :param user: CustomUser  instance
+        :return: True if the product is in the user's wishlist, False otherwise
+        """
+        # Check if the user has a wishlist and if the product is in it
+        try:
+            wishlist = WishListProduct.objects.get(user=user)
+            return self in wishlist.products.all()
+        except WishListProduct.DoesNotExist:
+            return False
 class Offer(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='offers')
     min_quantity = models.PositiveIntegerField()
