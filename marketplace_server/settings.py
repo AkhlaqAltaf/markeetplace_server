069375d093bd279911eab85de91c0d1983cf75c0
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2025-03-01 11:03:05 +0500

    STATIC ROOT CHANGE

--- marketplace_server/settings.py
+++ marketplace_server/settings.py
@@ -165,10 +165,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
-STATIC_ROOT = '/root/markeetplace_server/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/root/markeetplace_server/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
